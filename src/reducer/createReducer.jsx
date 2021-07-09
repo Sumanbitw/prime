eb14e70e45a96602a1d4bbba78597b70f75084ea
Sumanbitw <007sumanghosh@gmail.com>
2021-07-09 18:05:20 +0530

    updated reducer

--- src/reducer/createReducer.jsx
+++ src/reducer/createReducer.jsx
@@ -11,14 +11,58 @@ export const reducer = ( state, action ) => {
         case "CREATE__PLAYLIST": 
             return {...state, playlist : state.playlist.concat(action.payload)}
 
+        case "ADD__OR__REMOVE__PLAYLIST":
+            const playlist = state.playlist.find(
+                playlistItem => playlistItem._id === action.payload.playlistId)
+            const isVideoInPlaylist = playlist.videos.find(
+                videoItem => videoItem._id === action.payload.videoId)
+            return isVideoInPlaylist 
+            ? removeVideFromPlaylist(state, action.payload.videoId, action.payload.playlistId)
+            : addVideoInPlaylist(state, action.payload.videoId, action.payload.playlistId)
+        
+
         case "ADD__PLAYLIST":
-            console.log(state.playlist)
-            return {...state, playlist : state.playlist.map(videoItem => {
-                return videoItem.id !== action.payload.id
-                ? videoItem
-                : {...videoItem, videos: videoItem.videos.concat(action.payload.videoId)}
-            })}
+           return{
+               ...state,
+               playlist : [
+                   ...state.playlist,
+                   {
+                       _id : action.payload._id,
+                       name : action.payload.name,
+                       videos : [action.payload.videoId]
+                   }
+               ]
+           }
         default :
          return state
     }
 }
+
+const addVideoInPlaylist = (state, videoId, playlistId) => ({
+        ...state,
+        playlist : state.playlist.map(
+            playlistItem => {
+                return playlistItem._id === playlistId
+        
+        ? {
+            ...playlistItem,
+            videos : [...playlistItem.videos, videoId]
+        }
+        : playlistItem
+    }
+    )
+})
+
+const removeVideFromPlaylist = ( state, videoId, playlistId ) => ({
+    ...state,
+    playlist : state.playist.map(playlistItem => {
+            return playlistItem._id === playlistId
+            ? {
+                ...playlistItem,
+                videos : playlistItem.videos.filter(videoItem =>
+                    videoItem._id !== videoId)
+            }
+            : playlistItem
+        })
+
+}) 
