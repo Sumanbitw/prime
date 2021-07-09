5e7a6e255cb37d1f0d47883e695ef06278a91134
Sumanbitw <007sumanghosh@gmail.com>
2021-07-09 17:51:58 +0530

    updated index

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from "react-router-dom"
 import  VideoPlayer from './context/videoContext';
+import { AuthProvider } from './context/authContext';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
+      <AuthProvider>
     <VideoPlayer>
     <App />
     </VideoPlayer>
+    </AuthProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
